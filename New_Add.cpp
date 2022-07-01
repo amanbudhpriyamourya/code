209cd84f3541e67279d3b4b0707053f16dded111
Aman Budhpriya Mourya <73774081+amanbudhpriyamourya@users.noreply.github.com>
2022-07-01 12:05:46 +0530

    Add files via upload

---
+++ New_Add.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    int a,b;
+
+    cout<<"Enter the value of A: ";
+    cin>>a;
+    cout<<"Enter the value of B: ";
+    cin>>b;
+    cout<<a+b;
+
+
+    return 0;
+}
