f2975149bd82216d9b8fd08e0363e6c39e3c4a51
Aman Budhpriya Mourya <73774081+amanbudhpriyamourya@users.noreply.github.com>
2022-07-02 22:35:27 +0530

    Add files via upload

---
+++ New_Subtract.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    int a, b;
+    cout<<"Enter Value A: "; 
+    cin>>a;
+    cout<<"Enter Value B :";
+    cin>>b;
+    cout<<a+b;
+
+
+
+    return 0;
+}
