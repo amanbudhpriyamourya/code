0ff0d3a0580413cfdb02727991de4c4335288251
Aman Budhpriya Mourya <73774081+amanbudhpriyamourya@users.noreply.github.com>
2022-07-04 22:52:15 +0530

    Add files via upload

---
+++ Binary_operator_p5.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int a,b;
+  cout<<"Enter the value of A:";
+  cin>>a;
+  cout<<"Enter the value of B:";
+  cin>>b;
+  cout<<a+b<<endl<<a-b<<endl<<a*b<<endl<<a/b<<endl<<a%b;
+  return 0;
+}
